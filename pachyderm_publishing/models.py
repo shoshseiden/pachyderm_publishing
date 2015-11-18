2d579b5e7145914ff7e131d65bf46ab03bfca0e5
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-11-17 20:21:58 -0700

    setup models for books

---
+++ pachyderm_publishing/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Author(models.Model):
+    '''
+    author_formatted_name will be written in last name, first name format in order
+    to go by last name.
+
+    author_name will be the normal listing on the book page.
+    '''
+    author_name = models.CharField(max_length=50)
+    author_formatted_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.author_name
+
+
+class Books(models.Model):
+    book_genre = models.ForeignKey(Genre)
+    book_author = models.ForeignKey(Author)
+    book_title = models.CharField(max_length=100)
+    book_year = models.IntegerField()
+    book_cover = models.ImageField(upload_to='book_covers')
+
+    def __str__(self):
+        return self.book_title
+        
