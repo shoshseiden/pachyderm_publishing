6e81ec28b80ee49f2cbecf03484ca482d51969fe
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-11-17 19:16:47 -0700

    added admin url.

--- pachyderm_publishing/urls.py
+++ pachyderm_publishing/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 
-urlpatterns = patterns(
+urlpatterns = [
     "",
+    url(r'^admin/', include(admin.site.urls)),
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-)
+]
