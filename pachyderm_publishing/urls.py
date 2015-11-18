366196b08649b9a4a2785028a22351323279a333
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-11-17 20:03:47 -0700

    fixed url problem

--- pachyderm_publishing/urls.py
+++ pachyderm_publishing/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, url, include
-from django.views.generic import TemplateView
+from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    "",
     url(r'^admin/', include(admin.site.urls)),
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
 ]
