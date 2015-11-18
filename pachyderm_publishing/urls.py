4822025aa228f1d6ecd30610261f5bcd2ef5f9b7
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-11-18 00:44:05 -0700

    Added pages for author and author library.

--- pachyderm_publishing/urls.py
+++ pachyderm_publishing/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r'^genre/$', views.GenreView.as_view(), name='genre'),
-    url(r'^genre/genre_library/(?P<genre_id>[0-9]+)/$', views.library_by_genre, name='genre_library'),
-    url(r'^book/(?P<book_id>[0-9]+)/$', views.book, name='book')
+    url(r'^genre/(?P<genre_id>[0-9]+)/$', views.library_by_genre, name='genre_library'),
+    url(r'^author/$', views.AuthorView.as_view(), name='author'),
+    url(r'^author/(?P<author_id>[0-9]+)/$', views.library_by_author, name='author_library'),
+    url(r'^book/(?P<book_id>[0-9]+)/$', views.book, name='book'),
 ]
--- pachyderm_publishing/views.py
+++ pachyderm_publishing/views.py
@@ -16,10 +16,25 @@ class GenreView(generic.ListView):
         return Genre.objects.order_by('genre_name')
 
 
+class AuthorView(generic.ListView):
+    template_name = "author.html"
+    context_object_name = "author_list"
+
+    def queryset(self):
+        return Author.objects.order_by('author_formatted_name')
+
+
 def library_by_genre(request, genre_id):
     template_name = "library_by_genre.html"
     selected_genre = get_object_or_404(Genre, pk=genre_id)
     ctx = {'genre': selected_genre}
+    return render(request, template_name, ctx)
+
+
+def library_by_author(request, author_id):
+    template_name = "library_by_author.html"
+    selected_author = get_object_or_404(Author, pk=author_id)
+    ctx = {'author': selected_author}
     return render(request, template_name, ctx)
 
 
