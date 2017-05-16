0a2c32ec9c124733d269fb7bfedf9df878abc373
Shosh Seiden <shosh.j.seiden@gmail.com>
2017-05-15 20:37:50 -0600

    added jquery for hover events in libraries

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -4,3 +4,19 @@ require("bootstrap");
 
 require("../less/site.less");
 require("../images/pachyderm_banner.jpg");
+
+// jQuery script for cover enlargement
+$(document).ready(function(){
+  $('#librarycontent img').hover(function() {
+    $(this).width("150px").height("200px");
+    $(this).css("border-color", "teal");
+    $(this).css("border-style", "solid");
+    $(this).css("border-width", "5px");
+  },
+  function(){
+    $(this).width("100px").height("150px");
+    $(this).css("border-color", "none");
+    $(this).css("border-style", "none");
+    $(this).css("border-width", "0px");
+  });
+});
