729caa0f2bff630155cf0617df6644356bd1b2d8
Shosh Seiden <shosh.j.seiden@gmail.com>
2017-06-17 18:53:04 -0600

    added borders to images

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -7,7 +7,7 @@ require("../images/pachyderm_banner.jpg");
 
 // jQuery script for cover enlargement
 $(document).ready(function(){
-  $('#librarycontent img').hover(function() {
+  $('#library_content img').hover(function() {
     $(this).width("150px").height("200px");
     $(this).css("border-color", "teal");
     $(this).css("border-style", "solid");
@@ -15,8 +15,15 @@ $(document).ready(function(){
   },
   function(){
     $(this).width("100px").height("150px");
-    $(this).css("border-color", "none");
-    $(this).css("border-style", "none");
-    $(this).css("border-width", "0px");
+    $(this).css("border-color", "black");
+    $(this).css("border-style", "solid");
+    $(this).css("border-width", "2px");
   });
+});
+
+// jQuery script for book cover photo border
+$(document).ready(function(){
+  $('#book_content img').css("border-style", "solid");
+  $('#book_content img').css("border-color", "black");
+  $('#book_content img').css("border-width", "5px");
 });
